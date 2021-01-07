b9f4f45ebbdb1df3a3bbfa7751327340bad77622
Robynleigharris <57621781+Robynleigharris@users.noreply.github.com>
2021-01-07 12:08:35 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function number(value) {
+  document.form.answer.value += value;
+}
+function cle(value) {
+  document.form.answer.value = value;
+}
+function evalua() {
+  document.form.answer.value = eval(document.form.answer.value);
+}
